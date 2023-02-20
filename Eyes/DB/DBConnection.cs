3543d09ee6cdb2b63d191597f24661300dacb2b9
h4irullin.adel@yandex.ru <h4irullin.adel@yandex.ru>
2023-02-20 13:20:47 +0300

    gotovo

---
+++ Eyes/DB/DBConnection.cs
@@ -0,0 +1,15 @@
+﻿using Eyes.DB;
+using Eyes.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eyes
+{
+    public class DBConnection
+    {
+        public static PseudoLopushookEntities connection = new PseudoLopushookEntities();
+    }
+}
--- Eyes/MainWindow.xaml.cs
+++ Eyes/MainWindow.xaml.cs
@@ -23,6 +23,14 @@ namespace Eyes
         public MainWindow()
         {
             InitializeComponent();
+
+            frame.Navigated += Frame_Navigated;
+            frame.NavigationService.Navigate(new AgentsList());
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            var pageContent = frame.Content;
         }
     }
 }
